ea377fb72872f6ca53e6ee8e050ec9c5ce9ec3b6
LilGonzz <lucasgonzzagas@gmail.com>
2023-02-11 17:47:51 -0300

    alteraçao no VoteDTO para inserir id dos mapeamentos

--- src/main/java/com/LilGonzz/sicredappvotation/model/DTOs/VoteDTO.java
+++ src/main/java/com/LilGonzz/sicredappvotation/model/DTOs/VoteDTO.java
@@ -1,5 +1,6 @@
 package com.LilGonzz.sicredappvotation.model.DTOs;
 
+import com.LilGonzz.sicredappvotation.model.Vote;
 import com.LilGonzz.sicredappvotation.model.enums.VoteSelection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,19 @@ import lombok.NoArgsConstructor;
 public class VoteDTO {
     private Integer id;
     private VoteSelection selection;
+    private Integer sessionVoteId;
+    private Integer associateId;
 
-    public VoteDTO(Integer id, VoteSelection selection) {
+    public VoteDTO(Integer id, VoteSelection selection, Integer sessionVoteId, Integer associateId) {
         this.id = id;
         this.selection = selection;
+        this.sessionVoteId = sessionVoteId;
+        this.associateId = associateId;
+    }
+    public VoteDTO(Vote vote){
+        id = vote.getId();
+        selection = vote.getSelection();
+        sessionVoteId = vote.getSession().getId();
+        associateId = vote.getAssociate().getId();
     }
 }
