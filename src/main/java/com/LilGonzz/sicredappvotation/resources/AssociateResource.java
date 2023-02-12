aaca214834b2d98ae9db5c79d5f643841b7be0c3
LilGonzz <lucasgonzzagas@gmail.com>
2023-02-12 20:45:38 -0300

    criaçao do arquivo de config da documentação e feito o mapeamento dos endpoints dos associados

--- src/main/java/com/LilGonzz/sicredappvotation/resources/AssociateResource.java
+++ src/main/java/com/LilGonzz/sicredappvotation/resources/AssociateResource.java
@@ -3,6 +3,7 @@ package com.LilGonzz.sicredappvotation.resources;
 import com.LilGonzz.sicredappvotation.model.Associate;
 import com.LilGonzz.sicredappvotation.model.DTOs.AssociateDTO;
 import com.LilGonzz.sicredappvotation.services.AssociateService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,30 +20,42 @@ public class AssociateResource {
 
     @Autowired
     AssociateService service;
+    @Operation(
+            description = "irá buscar todos associados no banco de dados"
+    )
     @GetMapping
     public ResponseEntity<List<AssociateDTO>> getAllAssociate(){
             List<AssociateDTO> associates = service.getAllAssociateDTO();
             return ResponseEntity.ok(associates);
     }
-
+    @Operation(
+            description = "irá buscar associado no banco de dados pelo id"
+    )
     @GetMapping("/{id}")
     public ResponseEntity<AssociateDTO> getAssociateById(@PathVariable final Integer id){
             AssociateDTO associate = service.getAssociateByIdDTO(id);
             return ResponseEntity.ok(associate);
     }
+    @Operation(
+            description = "criará um associado na base de dados"
+    )
     @PostMapping
     public ResponseEntity<Associate> createAssociate(@RequestBody final AssociateDTO associateDto){
         Associate associate = service.createAssociate(associateDto);
         URI uri = URI.create("/associates/" + associate.getId());
         return ResponseEntity.created(uri).body(associate);
     }
-
+    @Operation(
+            description = "desativará o associado pelo id no banco de dados"
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<AssociateDTO> softDeteleAssociate(@PathVariable final Integer id){
        AssociateDTO associate = service.softDeleteAssociate(id);
        return ResponseEntity.ok(associate);
     }
-
+    @Operation(
+            description = "deleterá o associado do banco de dados"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<Void> hardDeleteAssociate(@PathVariable final Integer id){
         service.hardDeleteAssociate(id);
